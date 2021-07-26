5d6a5b53c0bbb59e200eb5f5fa748521f2b444f0
usurAndrei <andrewdrew22@outlook.com>
2021-07-26 20:37:07 +0300

    Updated UI, Firebase Connection

--- ChildApp/app/src/main/java/com/example/childapp/MainActivity.java
+++ ChildApp/app/src/main/java/com/example/childapp/MainActivity.java
@@ -2,14 +2,26 @@ package com.example.childapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //this.getSupportActionBar().hide(); // Hide Action Bar
         setContentView(R.layout.activity_main);
+    }
+
+    // Click to Enable
+    public void clickButtonEnable(View view) {
+
+    }
+
+    // Click to Disable
+    public void clickButtonDisable(View view) {
+
     }
 }
