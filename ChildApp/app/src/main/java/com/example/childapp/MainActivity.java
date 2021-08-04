d562d6b1ee1feb25bcf2e7df89a0996069d9c9e4
usurAndrei <andrewdrew22@outlook.com>
2021-08-04 14:58:08 +0300

    Implement OnClickListener, register buttons

--- ChildApp/app/src/main/java/com/example/childapp/MainActivity.java
+++ ChildApp/app/src/main/java/com/example/childapp/MainActivity.java
@@ -7,21 +7,35 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button BtEnable;
+    Button BtDisable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BtEnable = (Button)findViewById(R.id.buttonEnable);
+        BtDisable = (Button)findViewById(R.id.buttonDisable);
+
+        BtEnable.setOnClickListener(this); // Register Enable Button
+        BtDisable.setOnClickListener(this); // Register Disable Button
     }
 
-    // Click to Enable
-    public void clickButtonEnable(View view) {
-
-    }
-
-    // Click to Disable
-    public void clickButtonDisable(View view) {
-
+    @Override
+    public void onClick(View v) {
+        // Get clicked view ID
+        switch (v.getId()) {
+            case R.id.buttonEnable:
+                // Do something when enable is clicked
+                break;
+            case R.id.buttonDisable:
+                // Do something when disable is clicked
+                break;
+            default:
+                break;
+        }
     }
 }
