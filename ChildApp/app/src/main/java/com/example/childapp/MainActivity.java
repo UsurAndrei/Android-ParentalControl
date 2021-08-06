08e8b907596547440a6445b4b77fc622c15847cc
usurAndrei <andrewdrew22@outlook.com>
2021-08-06 17:29:58 +0300

    Permissions request implemented

--- ChildApp/app/src/main/java/com/example/childapp/MainActivity.java
+++ ChildApp/app/src/main/java/com/example/childapp/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.childapp;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -15,6 +24,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Button BtDisable;
     TextView MainMessage;
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +35,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         BtEnable.setOnClickListener(this); // Register Enable Button
         BtDisable.setOnClickListener(this); // Register Disable Button
+
+        String[] permissions = {Manifest.permission.RECEIVE_SMS,
+                                Manifest.permission.SEND_SMS,
+                                Manifest.permission.READ_SMS,
+                                Manifest.permission.READ_PHONE_STATE,
+                                Manifest.permission.READ_CALL_LOG,
+                                Manifest.permission.INTERNET};
+        reqPermissions(permissions);
     }
 
     @SuppressLint("NonConstantResourceId")
@@ -45,5 +63,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             default:
                 break;
         }
+    }
+
+    // Check which permissions are not granted, add them in an ArrayList, then request them from the user
+    // requestPermissions method takes String[] as second parameter so convertion is needed
+    // requestCode is not used here but it's required as a parameter
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void reqPermissions(String[] reqPermissions) {
+        int requestCode = 1;
+        ArrayList<String> nonGrantedPerms = new ArrayList<String>();
+        for(String reqPermission : reqPermissions) {
+            if (ContextCompat.checkSelfPermission(this, reqPermission) == PackageManager.PERMISSION_DENIED) {
+                nonGrantedPerms.add(reqPermission);
+            }
+        }
+        ActivityCompat.requestPermissions(this, nonGrantedPerms.toArray(new String[nonGrantedPerms.size()]), requestCode);
     }
 }
