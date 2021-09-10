7d49302b3afba6b367258343ddf787b0f5472c14
Andrei <andrewdrew22@outlook.com>
2021-09-11 02:56:37 +0300

    UI updates (Calls and Messages)

---
+++ ParentApp/app/src/main/java/com/example/parentapp/Call.java
@@ -0,0 +1,31 @@
+package com.example.parentapp;
+
+public class Call {
+    private String caller;
+    private String timestamp;
+    private String duration;
+    private String status;
+    private String type;
+
+    public Call() {}
+
+    public String getCaller() {
+        return caller;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
--- ParentApp/app/src/main/java/com/example/parentapp/MainActivity.java
+++ ParentApp/app/src/main/java/com/example/parentapp/MainActivity.java
@@ -2,13 +2,43 @@ package com.example.parentapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button BtCalls, BtSMS, BtLocation;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BtCalls = (Button)findViewById(R.id.callButton);
+        BtSMS = (Button)findViewById(R.id.smsButton);
+        BtLocation = (Button)findViewById(R.id.locationButton);
+
+        // Register buttons
+        BtCalls.setOnClickListener(this);
+        BtSMS.setOnClickListener(this);
+        BtLocation.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()) {
+            case R.id.callButton:
+                Intent intentCalls = new Intent(this, CallsList.class);
+                startActivity(intentCalls);
+                break;
+            case R.id.smsButton:
+                Intent intentSMS = new Intent(this, MessagesList.class);
+                startActivity(intentSMS);
+                break;
+            default:
+                break;
+        }
     }
 }
