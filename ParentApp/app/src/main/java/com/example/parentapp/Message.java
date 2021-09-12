53e262aff94f385a595165cf6bc845c07eb6788f
Andrei <andrewdrew22@outlook.com>
2021-09-13 02:21:08 +0300

    Small change to UI, added Message class

---
+++ ParentApp/app/src/main/java/com/example/parentapp/Message.java
@@ -0,0 +1,18 @@
+package com.example.parentapp;
+
+public class Message {
+    private String message;
+    private String number;
+    private String timestamp;
+    private String type;
+
+    public Message() {}
+
+    public String getMessage() { return message; }
+
+    public String getNumber() { return number; }
+
+    public String getTimestamp() { return timestamp; }
+
+    public String getType() { return type; }
+}
---
+++ ParentApp/app/src/main/java/com/example/parentapp/MessageAdapter.java
@@ -0,0 +1,4 @@
+package com.example.parentapp;
+
+public class MessageAdapter {
+}
