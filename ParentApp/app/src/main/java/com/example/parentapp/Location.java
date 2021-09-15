ba4ec0e4cfe2e0fdc830366ffde3d6a43b46d5fe
Andrei <andrewdrew22@outlook.com>
2021-09-16 02:52:21 +0300

    Location class created

---
+++ ParentApp/app/src/main/java/com/example/parentapp/Location.java
@@ -0,0 +1,21 @@
+package com.example.parentapp;
+
+public class Location {
+    private Double latitude;
+    private Double longitude;
+    private String timestamp;
+
+    public Location() {}
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+}
